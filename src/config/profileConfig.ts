eb3c9550a24b2d1fcdaa3d9b47bd45659675d2f4
猫星河 <130260637+maoxinhe@users.noreply.github.com>
2026-08-03 19:58:48 +0800

    更新 profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,8 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://q.qlogo.cn/headimg_dl?dst_uin=1096550598&spec=640&img_type=jpg
+",
 
 	// 名字
 	name: "Catkinr",
